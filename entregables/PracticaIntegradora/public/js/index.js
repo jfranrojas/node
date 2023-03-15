105c76070f892dbe38e34a8e06933c97fe0fe5e4
Francisco Rojas <em.franciscorojas.3h1@gmail.com>
2023-03-15 00:26:38 -0300

    index.js

---
+++ entregables/PracticaIntegradora/public/js/index.js
@@ -0,0 +1,58 @@
+const { application, json } = require("express");
+
+const productsContainer = document.getElementById("products")
+const form = document.getElementById('formProduct');
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    submitForm();
+});
+
+const submitForm = async () =>{
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+    const code = document.getElementById('code').value;
+    const price = document.getElementById('price').value;
+    const category = document.getElementById('category').value; 
+    const stock = document.getElementById('stock').value; 
+    const img = document.getElementById('img').value; 
+
+    await fetch(`${window.location.href}`, {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            title: title,
+            description: description,
+            code: code,
+            img: img,
+            price: price,
+            stock: stock,
+            category: category,
+            thumbnail: img,
+
+        }),
+
+    })
+    location.reload();
+};
+
+const deleteProduct = async (id) =>{
+    await fetch(`${window.location.href}/${id}`,{
+        method:"DELETE",
+        mode: "cors",
+        cache: "no-cache",
+        headers: {
+            "Content-Type" : "application/json",
+        },
+    });
+    location.reload();
+}
+
+const nextPage = async(page) => {
+    let nextPage = page++
+    moveUrl(nextPage)
+}
